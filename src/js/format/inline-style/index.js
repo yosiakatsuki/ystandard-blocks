1e4496e099ab2c52a3a4f44e940538a2aa07b7cb
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2019-08-23 12:57:32 +0900

    インライン装飾ボタン追加まで

--- src/js/format/inline-style/index.js
+++ src/js/format/inline-style/index.js
@@ -7,27 +7,25 @@ const {BlockFormatControls} = wp.editor;
 const inlineStyleList = [
     {
         name: 'ystdb/inline-style-1',
-        title: __('ユーザースタイル 1', 'ystandard-blocks'),
+        title: __('[ys]インラインスタイル 1', 'ystandard-blocks'),
         class: 'ystdb-inline--1',
         icon: 'admin-customizer'
+    },
+    {
+        name: 'ystdb/inline-style-2',
+        title: __('[ys]インラインスタイル 2', 'ystandard-blocks'),
+        class: 'ystdb-inline--2',
+        icon: 'admin-customizer'
+    },
+    {
+        name: 'ystdb/inline-style-3',
+        title: __('[ys]インラインスタイル 3', 'ystandard-blocks'),
+        class: 'ystdb-inline--3',
+        icon: 'admin-customizer'
     }
-    // ,
-    // {
-    //     name: 'ystdb/inline-style-2',
-    //     title: __('ユーザースタイル 2', 'ystandard-blocks'),
-    //     class: 'ystdb-inline--2',
-    //     icon: 'admin-customizer'
-    // },
-    // {
-    //     name: 'ystdb/inline-style-3',
-    //     title: __('ユーザースタイル 3', 'ystandard-blocks'),
-    //     class: 'ystdb-inline--3',
-    //     icon: 'admin-customizer'
-    // }
 ];
 
-inlineStyleList.map((list) => {
-    console.log(list);
+inlineStyleList.map((list, index) => {
     registerFormatType(list.name, {
         title: list.title,
         tagName: 'span',
@@ -39,14 +37,16 @@ inlineStyleList.map((list) => {
                 <Fragment>
                     <BlockFormatControls>
                         <div className="editor-format-toolbar block-editor-format-toolbar">
-                            <Toolbar>
-                                <ToolbarButton
-                                    icon={list.icon}
-                                    title={list.title}
-                                    onClick={onToggle}
-                                    isActive={isActive}
-                                />
-                            </Toolbar>
+                            <div className={`ystdb-inline-style-toolbar inline-style-${(index + 1)}`}>
+                                <Toolbar>
+                                    <ToolbarButton
+                                        icon={list.icon}
+                                        title={list.title}
+                                        onClick={onToggle}
+                                        isActive={isActive}
+                                    />
+                                </Toolbar>
+                            </div>
                         </div>
                     </BlockFormatControls>
                 </Fragment>
