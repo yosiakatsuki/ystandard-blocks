226eb8ec4fd842f343652dab364b8fca492e2bd8
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2019-12-20 13:09:26 +0900

    テスト追加

---
+++ tests/test-example.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Class Example_Test
+ *
+ * @package ystandard_blocks
+ */
+
+/**
+ * テスト
+ */
+class Example_Test extends WP_UnitTestCase {
+
+	/**
+	 * テスト
+	 */
+	function test_example() {
+		$this->assertTrue( true );
+	}
+}
