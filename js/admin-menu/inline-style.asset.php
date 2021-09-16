9649a1b66b9b075c7fc9eb3b8c30c58900ab1cf2
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2021-09-16 11:10:43 +0900

    プレビュー表示調整

--- js/admin-menu/inline-style.asset.php
+++ js/admin-menu/inline-style.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '3738297292bd6e7b406eb29bc3def664');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '012d2097eb9b58344f94b20dbd17bacc');
