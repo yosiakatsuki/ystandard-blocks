fe41be91d987d137d6f6d29be247620f0330ee82
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2022-06-06 16:43:16 +0900

    ref #77

--- blocks/conditional-group-block/edit.js
+++ blocks/conditional-group-block/edit.js
@@ -2,19 +2,39 @@ import classnames from 'classnames';
 /**
  * WordPress.
  */
-import { useInnerBlocksProps, useBlockProps } from '@wordpress/block-editor';
+import {
+	useInnerBlocksProps,
+	useBlockProps,
+	InnerBlocks,
+} from '@wordpress/block-editor';
+import { useSelect } from '@wordpress/data';
 /**
  * Block.
  */
 import GroupInspectorControls from './inspector-controls';
 
 const ConditionalGroupEdit = ( props ) => {
+	const { hasInnerBlocks } = useSelect( ( select ) => {
+		const { getBlock } = select( 'core/block-editor' );
+
+		const block = getBlock( props?.clientId );
+		return {
+			hasInnerBlocks: block && block.innerBlocks.length,
+		};
+	} );
 	const blockProps = useBlockProps( {
 		className: classnames( 'ystdb-conditional-group' ),
 	} );
-	const innerBlocksProps = useInnerBlocksProps( {
-		className: classnames( 'ystdb-conditional-group__inner' ),
-	} );
+	const innerBlocksProps = useInnerBlocksProps(
+		{
+			className: classnames( 'ystdb-conditional-group__inner' ),
+		},
+		{
+			renderAppender: hasInnerBlocks
+				? undefined
+				: () => <InnerBlocks.ButtonBlockAppender />,
+		}
+	);
 
 	return (
 		<>
--- js/blocks/conditional-group-block.asset.php
+++ js/blocks/conditional-group-block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '00758e9948352e36c5d1');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'f48e89c81052aabebaad');
