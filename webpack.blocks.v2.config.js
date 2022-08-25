0d26ec999e64e8be036cefb12de341894403b77c
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2022-08-25 17:13:07 +0900

    Create webpack.blocks.v2.config.js

---
+++ webpack.blocks.v2.config.js
@@ -0,0 +1,18 @@
+const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+const path = require( 'path' );
+
+module.exports = {
+	...defaultConfig,
+	resolve: {
+		...defaultConfig.resolve,
+		alias: {
+			...defaultConfig.resolve.alias,
+			'@aktk/config': path.resolve( __dirname, 'src/js/config' ),
+			'@aktk/components': path.resolve( __dirname, 'src/js/components' ),
+			'@aktk/controls': path.resolve( __dirname, 'src/js/controls' ),
+			'@aktk/helper': path.resolve( __dirname, 'src/js/helper' ),
+			'@aktk/util': path.resolve( __dirname, 'src/js/util' ),
+		},
+	},
+	cache: false,
+};
