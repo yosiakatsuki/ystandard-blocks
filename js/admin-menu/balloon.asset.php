0d5692bd9fe4cf006bbba9c2122e30928bce6314
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2023-03-30 10:49:55 +0900

    インラインスタイル編集機能：設定画面スタイル調整

--- js/admin-menu/balloon.asset.php
+++ js/admin-menu/balloon.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-primitives'), 'version' => '512a644ca063d55498aa');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-primitives'), 'version' => '09b035a369c04e24b788');
--- js/admin-menu/inline-style.asset.php
+++ js/admin-menu/inline-style.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '85cf11f0e872b758abae');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'ed5e734c2a5ba974b075');
