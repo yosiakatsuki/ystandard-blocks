eb0c84f5aaa20966512556ec19b4f7926395717a
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2023-11-08 16:33:44 +0900

    fix lint

--- src/js/components/svg-icon-select/icon-select.js
+++ src/js/components/svg-icon-select/icon-select.js
@@ -1,98 +1,88 @@
-import { Component, useState } from '@wordpress/element';
+import { useState } from '@wordpress/element';
 import { Button } from '@wordpress/components';
 import classnames from 'classnames';
 import { getIcons } from '@aktk/util/icons';
 import SVGIcon from '@aktk/components/svg-icon';
 import { __ } from '@wordpress/i18n';
 
-class IconSelect extends Component {
-	render() {
-		const { selectedIcon, onChange, previewIcon } = this.props;
+const IconSelect = (props) => {
+	const { selectedIcon, onChange, previewIcon } = props;
 
-		const [isOpen, setIsOpen] = useState(false);
+	const [isOpen, setIsOpen] = useState(false);
 
-		const iconList = getIcons();
-		const isPreview = false === previewIcon ? previewIcon : true;
+	const iconList = getIcons();
+	const isPreview = false === previewIcon ? previewIcon : true;
 
-		return (
-			<div className={'ys-icon-picker'}>
-				<div className={'ys-icon-picker__selected'}>
-					{isPreview && (
-						<div className={'ys-icon-picker__preview'}>
-							{!!selectedIcon && <SVGIcon name={selectedIcon} />}
-						</div>
-					)}
-					<Button
-						className={classnames({
-							'is-open': isOpen,
-						})}
-						isSecondary
-						onClick={() => {
-							setIsOpen(!isOpen);
-						}}
-						style={{
-							minWidth: '110px',
-							textAlign: 'center',
-							justifyContent: 'center',
-						}}
-					>
-						{isOpen && (
-							<span>{__('閉じる', 'ystandard-blocks')}</span>
-						)}
-						{!isOpen && (
-							<span>
-								{__('アイコン選択', 'ystandard-blocks')}
-							</span>
-						)}
-					</Button>
-					<Button
-						className={'ys-icon-picker__remove'}
-						disabled={!selectedIcon}
-						isSmall
-						isSecondary
-						onClick={() => {
-							onChange('');
-							setIsOpen(false);
-						}}
-					>
-						{__('リセット', 'ystandard-blocks')}
-					</Button>
-				</div>
-				<div
-					className={classnames('ys-icon-picker__list', {
+	return (
+		<div className={'ys-icon-picker'}>
+			<div className={'ys-icon-picker__selected'}>
+				{isPreview && (
+					<div className={'ys-icon-picker__preview'}>
+						{!!selectedIcon && <SVGIcon name={selectedIcon} />}
+					</div>
+				)}
+				<Button
+					className={classnames({
 						'is-open': isOpen,
 					})}
+					isSecondary
+					onClick={() => {
+						setIsOpen(!isOpen);
+					}}
+					style={{
+						minWidth: '110px',
+						textAlign: 'center',
+						justifyContent: 'center',
+					}}
 				>
-					<div className="ys-icon-picker__list-container">
-						{iconList.map((icon) => {
-							return (
-								<Button
-									key={icon.name}
-									className={classnames(
-										`ys-icon-picker__icon`,
-										{
-											'is-selected':
-												selectedIcon === icon.name,
-										}
-									)}
-									onClick={() => {
-										if (selectedIcon === icon.name) {
-											onChange('');
-											setIsOpen(false);
-										} else {
-											onChange(icon.name);
-										}
-									}}
-								>
-									<SVGIcon name={icon.name} />
-								</Button>
-							);
-						})}
-					</div>
+					{isOpen && <span>{__('閉じる', 'ystandard-blocks')}</span>}
+					{!isOpen && (
+						<span>{__('アイコン選択', 'ystandard-blocks')}</span>
+					)}
+				</Button>
+				<Button
+					className={'ys-icon-picker__remove'}
+					disabled={!selectedIcon}
+					isSmall
+					isSecondary
+					onClick={() => {
+						onChange('');
+						setIsOpen(false);
+					}}
+				>
+					{__('リセット', 'ystandard-blocks')}
+				</Button>
+			</div>
+			<div
+				className={classnames('ys-icon-picker__list', {
+					'is-open': isOpen,
+				})}
+			>
+				<div className="ys-icon-picker__list-container">
+					{iconList.map((icon) => {
+						return (
+							<Button
+								key={icon.name}
+								className={classnames(`ys-icon-picker__icon`, {
+									'is-selected': selectedIcon === icon.name,
+								})}
+								onClick={() => {
+									if (selectedIcon === icon.name) {
+										onChange('');
+										setIsOpen(false);
+									} else {
+										onChange(icon.name);
+									}
+								}}
+							>
+								<SVGIcon name={icon.name} />
+							</Button>
+						);
+					})}
 				</div>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
 
 export default IconSelect;
