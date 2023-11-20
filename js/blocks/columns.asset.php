d83c23bdd05e36e88229723cd5cbf7a111ee81d3
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2023-11-20 22:18:01 +0900

    旧カラムブロック削除

--- js/blocks/columns.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '93a168d4339136570747');
