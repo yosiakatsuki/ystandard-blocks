184106fedcd20f1eda8da5f141a2c7224b4bc219
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2023-11-21 13:03:08 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import("tailwindcss").Config} */
 module.exports = {
 	content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+	corePlugins: {
+		preflight: false,
+	},
 	theme: {
 		extend: {
 			colors: {
