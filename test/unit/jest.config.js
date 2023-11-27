0446b8c55e9d207bb830848c6cb2d7e7bf159080
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2023-11-27 15:56:03 +0900

    js test 旧コンポーネント参照調整

--- test/unit/jest.config.js
+++ test/unit/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
 	rootDir: '../../',
 	moduleNameMapper: {
 		'^@aktk/blocks/(.+)': '<rootDir>/src/blocks/$1',
+		'^@aktk/blocks-old/(.+)': '<rootDir>/src/js/$1',
 		'^@aktk/(.+)': '<rootDir>/src/js/$1',
 	},
 	preset: '@wordpress/jest-preset-default',
