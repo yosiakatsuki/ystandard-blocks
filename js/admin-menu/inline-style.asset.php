e07dbf3ee70be09d64bce8627a495c2900e4ff28
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2024-04-05 09:49:14 +0900

    カスタム見出しブロック：編集画面スタイル調整

--- js/admin-menu/inline-style.asset.php
+++ js/admin-menu/inline-style.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '44371888b236614751b1');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'd30ab78af50257cf994d');
