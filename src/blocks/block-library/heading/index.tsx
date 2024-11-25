8c6140b50dcdc550fecbb3e17e44e812b334f040
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2024-11-25 12:29:24 +0900

    見出しブロック：リニューアル　基本ファイル作成

---
+++ src/blocks/block-library/heading/index.tsx
@@ -0,0 +1,51 @@
+import { Bookmark } from 'react-feather';
+/**
+ * WordPress Dependencies.
+ */
+import { registerBlockType } from '@wordpress/blocks';
+/**
+ * Aktk Dependencies.
+ */
+import { COLORS } from '@aktk/block-components/config';
+/**
+ * Plugin.
+ */
+import { CATEGORY } from '@aktk/blocks/config';
+import { mergeDefaultAttributes } from '@aktk/blocks/utils';
+/**
+ * Block
+ */
+// @ts-ignore
+import metadata from './block.json';
+import edit from './edit';
+import save from './save';
+import './style.scss';
+
+/**
+ * Register custom heading block.
+ */
+function registerCustomHeadingBlock() {
+	const attributes = mergeDefaultAttributes(
+		metadata.name,
+		metadata.attributes
+	);
+	// @ts-ignore
+	registerBlockType( metadata.name, {
+		...metadata,
+		...{
+			icon: (
+				<Bookmark
+					stroke={ COLORS.iconForeground }
+					style={ { fill: 'none' } }
+				/>
+			),
+			category: CATEGORY.common,
+			attributes,
+			edit,
+			save,
+			example: {},
+		},
+	} );
+}
+
+registerCustomHeadingBlock();
