fde3e342108689a05f9b0c018b4c14f05ebebfc3
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2025-04-22 12:56:31 +0900

    ボタンブロックfontSize入力調整

--- src/blocks/block-library/button/inspector-controls/typography/font-size.tsx
+++ src/blocks/block-library/button/inspector-controls/typography/font-size.tsx
@@ -3,41 +3,37 @@
  */
 import { __ } from '@wordpress/i18n';
 /**
- * Plugin dependencies.
+ * Aktk dependencies.
  */
 import {
-	ResponsiveFontSizeSelect,
-	ResponsiveFontSize,
-} from '@aktk/blocks/components/responsive-font-size';
+	CustomFontSizePicker,
+	type CustomFontSizePickerOnChangeProps,
+} from '@aktk/block-components/components/custom-font-size-picker';
+import BaseControl from '@aktk/block-components/wp-controls/base-control';
 
 // @ts-expect-error
 export function FontSize( props ) {
 	const { attributes, setAttributes } = props;
 	const { fontSize, customFontSize, responsiveFontSize } = attributes;
-	const handleOnResponsiveChange = (
-		value: ResponsiveFontSize | undefined
-	) => {
+
+	const handleOnChange = ( value: CustomFontSizePickerOnChangeProps ) => {
 		setAttributes( {
-			responsiveFontSize: value,
-			customFontSize: undefined,
-			fontSize: undefined,
+			fontSize: value?.fontSize,
+			customFontSize: value?.customFontSize,
+			responsiveFontSize: value?.responsiveFontSize,
 		} );
 	};
 	return (
-		<>
-			<ResponsiveFontSizeSelect
-				label={ __( '文字サイズ', 'ystandard-blocks' ) }
-				value={ customFontSize || fontSize }
-				onPickerChange={ ( size, slug ) => {
-					setAttributes( {
-						customFontSize: size,
-						fontSize: slug,
-						responsiveFontSize: undefined,
-					} );
-				} }
-				responsiveValue={ responsiveFontSize }
-				onResponsiveChange={ handleOnResponsiveChange }
+		<BaseControl
+			id={ 'font-size' }
+			label={ __( 'フォントサイズ', 'ystandard-toolbox' ) }
+		>
+			<CustomFontSizePicker
+				fontSize={ fontSize }
+				customFontSize={ customFontSize }
+				responsiveFontSize={ responsiveFontSize }
+				onChange={ handleOnChange }
 			/>
-		</>
+		</BaseControl>
 	);
 }
