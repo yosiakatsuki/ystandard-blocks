0cc277c7eb592a2dae6bbe4d92e5215ec21fe50e
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2025-04-25 10:58:48 +0900

    New Button：テキスト関連設定の更新

--- src/blocks/block-library/button/inspector-controls/typography/text-color.tsx
+++ src/blocks/block-library/button/inspector-controls/typography/text-color.tsx
@@ -4,6 +4,11 @@
 import { __ } from '@wordpress/i18n';
 
 /**
+ * Aktk dependencies.
+ */
+import BaseControl from '@aktk/block-components/wp-controls/base-control';
+
+/**
  * Plugin dependencies.
  */
 import { ColorGradientSettingsDropdown } from '@aktk/blocks/components/color';
@@ -13,13 +18,15 @@ export function TextColor( props ) {
 	const { textColor, setTextColor } = props;
 	return (
 		<>
-			<ColorGradientSettingsDropdown
-				controlLabel={ __( '文字色', 'ystandard-blocks' ) }
-				colorValue={ textColor.color }
-				onChange={ setTextColor }
-				label={ __( '文字色', 'ystandard-blocks' ) }
-				disableGradient={ true }
-			/>
+			<BaseControl>
+				<ColorGradientSettingsDropdown
+					controlLabel={ __( '文字色', 'ystandard-blocks' ) }
+					colorValue={ textColor.color }
+					onChange={ setTextColor }
+					label={ __( '文字色', 'ystandard-blocks' ) }
+					disableGradient={ true }
+				/>
+			</BaseControl>
 		</>
 	);
 }
