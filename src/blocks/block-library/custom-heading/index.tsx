0792c95f14a169d74f02358710ed819b06ca0fc7
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2025-11-03 18:00:27 +0900

    新見出し：transformを別ファイルに変更

--- src/blocks/block-library/custom-heading/index.tsx
+++ src/blocks/block-library/custom-heading/index.tsx
@@ -2,7 +2,7 @@ import { Bookmark } from 'react-feather';
 /**
  * WordPress.
  */
-import { registerBlockType, createBlock } from '@wordpress/blocks';
+import { registerBlockType } from '@wordpress/blocks';
 /**
  * Aktk Dependencies.
  */
@@ -20,84 +20,8 @@ import { mergeDefaultAttributes } from '@aktk/blocks/utils';
 import metadata from './block.json';
 import edit from './edit';
 import save from './save';
+import { transforms } from './transform';
 import './style.scss';
-
-/**
- * ブロック変換定義
- */
-const transforms = {
-	from: [
-		// core/heading からの変換
-		{
-			type: 'block',
-			blocks: [ 'core/heading' ],
-			transform: ( attributes: any ) => {
-				return createBlock( metadata.name, {
-					content: attributes.content,
-					level: attributes.level,
-					textAlign: attributes.textAlign,
-					textColor: attributes.textColor,
-					customTextColor: attributes.customTextColor,
-					fontSize: attributes.fontSize,
-					customFontSize: attributes.customFontSize,
-				} );
-			},
-		},
-		// ystdb/heading からの変換
-		{
-			type: 'block',
-			blocks: [ 'ystdb/heading' ],
-			transform: ( attributes: any ) => {
-				// responsiveFontSize オブジェクトの構築
-				const responsiveFontSize: any = {};
-				if ( attributes.useFontSizeResponsive ) {
-					if ( attributes.fontSizeMobile ) {
-						responsiveFontSize.mobile = attributes.fontSizeMobile;
-					}
-					if ( attributes.fontSizeTablet ) {
-						responsiveFontSize.tablet = attributes.fontSizeTablet;
-					}
-					if ( attributes.fontSizeDesktop ) {
-						responsiveFontSize.desktop = attributes.fontSizeDesktop;
-					}
-				}
-
-				return createBlock( metadata.name, {
-					content: attributes.content,
-					level: attributes.level,
-					textAlign: attributes.align, // align -> textAlign
-					textColor: attributes.textColor,
-					customTextColor: attributes.customTextColor,
-					fontSize: attributes.fontSize,
-					customFontSize: attributes.customFontSize,
-					responsiveFontSize:
-						Object.keys( responsiveFontSize ).length > 0
-							? responsiveFontSize
-							: undefined,
-					fontWeight: attributes.fontWeight,
-					letterSpacing: attributes.letterSpacing
-						? String( attributes.letterSpacing )
-						: undefined,
-					clearStyle: attributes.clearStyle,
-				} );
-			},
-		},
-	],
-	to: [
-		// core/heading への変換
-		{
-			type: 'block',
-			blocks: [ 'core/heading' ],
-			transform: ( attributes: any ) => {
-				return createBlock( 'core/heading', {
-					content: attributes.content,
-					level: attributes.level,
-					textAlign: attributes.textAlign,
-				} );
-			},
-		},
-	],
-};
 
 export function registerCustomHeadingBlock() {
 	const blockAttributes = mergeDefaultAttributes(
---
+++ src/blocks/block-library/custom-heading/transform.tsx
@@ -0,0 +1,87 @@
+/**
+ * WordPress dependencies.
+ */
+import { createBlock } from '@wordpress/blocks';
+
+/**
+ * Block
+ */
+// @ts-ignore
+import metadata from './block.json';
+
+/**
+ * ブロック変換定義
+ */
+export const transforms = {
+	from: [
+		// core/heading からの変換
+		{
+			type: 'block',
+			blocks: [ 'core/heading' ],
+			transform: ( attributes: any ) => {
+				return createBlock( metadata.name, {
+					content: attributes.content,
+					level: attributes.level,
+					textAlign: attributes.textAlign,
+					textColor: attributes.textColor,
+					customTextColor: attributes.customTextColor,
+					fontSize: attributes.fontSize,
+					customFontSize: attributes.customFontSize,
+				} );
+			},
+		},
+		// ystdb/heading からの変換
+		{
+			type: 'block',
+			blocks: [ 'ystdb/heading' ],
+			transform: ( attributes: any ) => {
+				// responsiveFontSize オブジェクトの構築
+				const responsiveFontSize: any = {};
+				if ( attributes.useFontSizeResponsive ) {
+					if ( attributes.fontSizeMobile ) {
+						responsiveFontSize.mobile = attributes.fontSizeMobile;
+					}
+					if ( attributes.fontSizeTablet ) {
+						responsiveFontSize.tablet = attributes.fontSizeTablet;
+					}
+					if ( attributes.fontSizeDesktop ) {
+						responsiveFontSize.desktop = attributes.fontSizeDesktop;
+					}
+				}
+
+				return createBlock( metadata.name, {
+					content: attributes.content,
+					level: attributes.level,
+					textAlign: attributes.align, // align -> textAlign
+					textColor: attributes.textColor,
+					customTextColor: attributes.customTextColor,
+					fontSize: attributes.fontSize,
+					customFontSize: attributes.customFontSize,
+					responsiveFontSize:
+						Object.keys( responsiveFontSize ).length > 0
+							? responsiveFontSize
+							: undefined,
+					fontWeight: attributes.fontWeight,
+					letterSpacing: attributes.letterSpacing
+						? String( attributes.letterSpacing )
+						: undefined,
+					clearStyle: attributes.clearStyle,
+				} );
+			},
+		},
+	],
+	to: [
+		// core/heading への変換
+		{
+			type: 'block',
+			blocks: [ 'core/heading' ],
+			transform: ( attributes: any ) => {
+				return createBlock( 'core/heading', {
+					content: attributes.content,
+					level: attributes.level,
+					textAlign: attributes.textAlign,
+				} );
+			},
+		},
+	],
+};
