f020b738e11e980d1d02d2f15df2b88bb8a313e4
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2026-02-05 10:22:55 +0900

    Update index.php

--- inc/blocks/index.php
+++ inc/blocks/index.php
@@ -5,4 +5,5 @@ require_once __DIR__ . '/class-block-utility.php';
 require_once __DIR__ . '/class-dynamic-block.php';
 require_once __DIR__ . '/class-color-palette.php';
 require_once __DIR__ . '/class-font-size.php';
+require_once __DIR__ . '/class-blocks.php';
 require_once __DIR__ . '/class-deprecated-blocks.php';
