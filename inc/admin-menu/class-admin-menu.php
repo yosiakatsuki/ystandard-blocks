92a40d1a9799887b81ede9c314adc279f78f7b85
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2026-02-05 10:20:24 +0900

    ブロック以外の rel='noopener noreferrer' 削除

--- inc/admin-menu/class-admin-menu.php
+++ inc/admin-menu/class-admin-menu.php
@@ -209,7 +209,7 @@ class Admin_Menu {
 									</a>
 								</div>
 								<div class="ystdb-menu-top__column no-shadow no-padding">
-									<a class="ystdb-menu-top__link-manual button is-primary is-small" href="https://wp-ystandard.com/manual/ystdb-inline-style/" target="_blank" rel="noopener">
+									<a class="ystdb-menu-top__link-manual button is-primary is-small" href="https://wp-ystandard.com/manual/ystdb-inline-style/" target="_blank">
 										<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-book">
 											<path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"></path>
 											<path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"></path>
@@ -245,7 +245,7 @@ class Admin_Menu {
 									</a>
 								</div>
 								<div class="ystdb-menu-top__column no-shadow no-padding">
-									<a class="ystdb-menu-top__link-manual button is-primary is-small" href="https://wp-ystandard.com/manual/ystdb-balloon-avatar-setting/" target="_blank" rel="noopener">
+									<a class="ystdb-menu-top__link-manual button is-primary is-small" href="https://wp-ystandard.com/manual/ystdb-balloon-avatar-setting/" target="_blank">
 										<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-book">
 											<path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"></path>
 											<path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"></path>
