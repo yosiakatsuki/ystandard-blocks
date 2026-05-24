b0c95c492a8e5532bfedac7d1cd96bd52e263c22
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2026-05-24 20:50:11 +0900

    レイアウトパネル　ベース作成

--- src/blocks/block-library/custom-heading/inspector-controls/index.tsx
+++ src/blocks/block-library/custom-heading/inspector-controls/index.tsx
@@ -6,6 +6,7 @@ import { InspectorControls as WPInspectorControls } from '@wordpress/block-edito
 /**
  * Block dependencies.
  */
+import { LayoutPanel } from './layout';
 import { MainTextPanel } from './main-text';
 import { ClearStylePanel } from './clear-style';
 
@@ -13,6 +14,7 @@ import { ClearStylePanel } from './clear-style';
 export function InspectorControls( props ) {
 	return (
 		<WPInspectorControls>
+			<LayoutPanel { ...props } />
 			<MainTextPanel { ...props } />
 			<ClearStylePanel { ...props } />
 		</WPInspectorControls>
---
+++ src/blocks/block-library/custom-heading/inspector-controls/layout/index.tsx
@@ -0,0 +1,18 @@
+/**
+ * WordPress dependencies.
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Aktk dependencies.
+ */
+import { Panel } from '@aktk/block-components/components/panel';
+
+// @ts-ignore.
+export function LayoutPanel( props ) {
+	return (
+		<Panel title={ __( 'レイアウト', 'ystandard-blocks' ) }>
+
+		</Panel>
+	);
+}
